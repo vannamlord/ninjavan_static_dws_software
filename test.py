c30e4f3dd7ca900d3ff52a8a54009e8a69d6d6a6
NJV <vdnam.ute@gmai.com>
2024-07-22 22:04:08 +0700

    update camera connect

--- test.py
+++
@@ -1,1 +0,0 @@
-volumeNum=2[0]: volume=33026846.000000, length=368.268494, width=359.057037, height=393.000000, center.x=-90.978867, center.y=30.693319, center.z=2140.790527[1]: volume=1767515.875000, length=200.961411, width=56.534222, height=186.000000, center.x=-7.446582, center.y=503.893066, center.z=2238.234619; ImgName is /20240722_190119_479.jpg
